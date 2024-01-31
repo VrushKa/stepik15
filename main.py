fe614215822d16629a454480ab9988ffec9ef25d
VrushKa <158237162+VrushKa@users.noreply.github.com>
2024-01-31 03:52:06 +0100

    Dodano testy jednostkowe dla funkcji dodawania

---
+++ main.py
@@ -0,0 +1,13 @@
+# test_calculator.py
+import unittest
+from game import add
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
+
